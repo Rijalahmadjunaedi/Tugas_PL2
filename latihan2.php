50432435f38fa7377d370f0a72e183bc3bf7c1aa
Rijalahmadjunaedi <rijalahmadjunaedi@gmail.com>
2021-08-20 00:10:59 +0700

    first commit

---
+++ latihan2.php
@@ -0,0 +1,25 @@
+<h1>Input Mahasiswa</h2>
+<?php
+        include 'koneksi.php';
+        $db = new Database();
+        $con=$db->Connect();
+
+        if(isset($_POST['proses']))
+        {
+            $query=mysqli_query($con, "INSERT into tabel_mhs values(
+            '".$_POST['nama']."',
+            '".$_POST['npm']."'
+        )");
+        header('location:latihan1.php');
+        
+        }
+?>
+
+<form action="" method="POST">
+    NPM <br>
+        <input type="text" name="npm"><br>
+    NAMA <br>    
+        <input type="text" name="nama"><br>
+        <input type="submit" name="proses" value="Simpan">
+        <input type='button' onclick=location.href='latihan1.php' value='Batal' />
+</form
